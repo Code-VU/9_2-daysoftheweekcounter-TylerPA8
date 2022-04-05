46605491d4996be88edef759e8ba2bb3b1ac872e
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-04-05 13:27:51 -0500

    updated dictionary

--- counter.py
+++ counter.py
@@ -1,7 +1,20 @@
 
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    file_name = input("Enter a file name: ")   
+    file_open = open(file_name)
+    fr = file_open.readlines()
+    froms ={}
+    for n in fr:
+        if n.startswith("From "):
+            n = n.strip()
+            n = n.split(' ')
+            if n[2] in froms:
+                froms[n[2]] += 1
+            else:
+                froms[n[2]] = 1
+    print(froms)
+    # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
